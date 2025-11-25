8c0765f27a67a856ba11ce96dd4e6efd75f3f9d1
benfach <hong125841@gmail.com>
2025-11-25 15:50:05 +0800

    refactor: restructure project folder and files for better organization, etc.

---
+++ Verve.UniEx/Runtime/Core/Common/Event/EventBus.cs
@@ -0,0 +1,17 @@
+#if UNITY_5_3_OR_NEWER
+
+namespace Verve.UniEx
+{
+    using System;
+    
+    
+    /// <summary>
+    ///   <para>事件总线</para>
+    /// </summary>
+    public class EventBus : InstanceBase<EventBus>
+    {
+
+    }
+}
+
+#endif
